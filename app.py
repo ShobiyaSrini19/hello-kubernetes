e20b2f1f63eadc719851c162a3376c569054287f
root <root@ASUS-Vivobook-16.localdomain>
2026-04-12 17:45:54 +0000

    Testing auto-trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from Jenkins + Kubernetes!</h1><p>Build Successful!</p>"
+    return "<h1>Hello devops!</h1><p>Build Successful!</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
